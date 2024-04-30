c7690a548f76fd619d7c97b6fbca95ab11362b3b
Ahmed Basamad <a.h.basamad.cn@gmail.com>
2024-04-30 13:46:45 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,26 @@ import PlanetsContainer from "./PlanetsContainer"
 
 function App() {
 
-  const [planets, setPlanets] = useState('')
+  const [planets, setPlanets] = useState([])
+  const [mode, setMode] = useState(true)
 
+  function toggle_mode(){
+    setMode(!mode)
+  }
   useEffect(() => {
     console.log('fetching planets...')
     fetch('http://localhost:4001/planets')
     .then(res => res.json())
     .then(data => setPlanets(data))
-  })
+  }, [])
 
   return (
-    <div className="App">
+    <div className={(!mode) ? "App light-mode" : "App"}>
 
       {/* You won't have to touch StarField, it's just here for visual things */}
       <StarField />
 
-      <button>Light Mode</button>
+      <button onClick={toggle_mode}>Light Mode</button>
 
       <PlanetsContainer planets={planets} />
 
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PlanetFacts from "./PlanetFacts"
+import { useState } from 'react'
 
 function PlanetCard({planet}) {
-
+  const [ toggle, setToggle ] = useState(false)
+  
+  function handleclick(){
+    setToggle(!toggle)
+  }
   return (
-    <div className="card" onClick={() => console.log('Toggle!')}>
+    <div className="card" onClick={handleclick}>
       <h2>{planet.name}</h2>
-      <img className="" src={planet.image} alt={planet.name} />
+      {(!toggle) ?
+      <img className={(planet.is_planet) ? null : "not_planet"} src={planet.image} alt={planet.name} />
+      :
+      <PlanetFacts planet={planet} />}
     </div>
   )
 }
